73e9a152d9eb93a30ac626b773b1cd405225d8c5
Manali Amin <31667682+ManaliAmin@users.noreply.github.com>
2017-09-06 17:24:13 -0400

    Final

---
+++ Assignment/Final/main.cpp
@@ -0,0 +1,17 @@
+// Final Project Milestone 5
+// The Wedding Planner application
+// File WPlannerMain.cpp
+// Version 1.0
+// Date 2015/08/10
+// Author Fardad Soleimanloo
+//
+// Revision History
+// -----------------------------------------------------------
+// Name               Date                 Reason
+//Manali Amin        2016/12/3            Wedding Planer
+/////////////////////////////////////////////////////////////////
+#include "WPlanner.h"
+int main(){
+	ict::WPlanner app("WPlanner.txt");
+	return app.run();	
+}
